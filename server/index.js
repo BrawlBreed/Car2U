1f7d303650e19e64cd876564c35fe350b6fe0b37
admin <zantonius20@gmail.com>
2025-04-18 12:53:39 -0400

    Commit before deployment

--- server/index.js
+++ server/index.js
@@ -22,6 +22,19 @@ app.use('/api/brand',brandRoutes);
 app.use('/api/car',carRoutes);
 app.use('/api/order',orderRoutes);
 
+app.use((req, res, next) => {
+    const allowedOrigins = ['https://car2-u.vercel.app', 'http://localhost:3000'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Source, Referer, Countrycode');
+  
+    next();
+});
+
 app.listen(process.env.PORT,() => {
     console.log('Car Running on port 5000');
 })
